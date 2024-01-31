fead3263e8d607b07cc7241b35fa0fac02713892
Achyutarya Raaz <59292426+Achyutaryaa@users.noreply.github.com>
2024-02-01 01:23:22 +0530

    Update main.js
    Modify name using js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 let date = new Date();
 let dateTag = document.getElementsByClassName("date");
 dateTag.innerHTML = date;
+
+
+
+const nameList = ["Achyutarya Raaz", "अच्युतआर्य राज़"];
+
+const nameField = document.querySelector(".name");
+
+nameField.innerHTML = nameList[1];
