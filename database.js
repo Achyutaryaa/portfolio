cee56d8d37142534cdf6db28ec471455757c6c08
Achyutarya Raaz <Achyutaryaa@users.noreply.github.com>
2024-12-01 12:35:14 +0530

    Contents
    I have added missing contents to relevant elements.

--- database.js
+++ database.js
@@ -4,3 +4,14 @@ const db = new database("database.db");
 const query = db.prepare("SELECT * FROM users WHERE name = ? AND age = ?");
 const user = query.get("Bob", 25);
 
+// after submitting service form
+
+function submitServiceForm() {
+    console.log("button clicked");
+    const serviceBody = document.querySelector("body");
+    serviceBody.innerHTML = "";
+    serviceBody.style.backgroundColor = "green";
+    const h2 = document.createElement("h2");
+    h2.textContent = "You have successfully submitted service request. I will contact soon...";
+    serviceBody.appendChild(h2);
+}
--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 // ==========================================================================================
-// Home
-
-
-// ==========================================================================================
 // top button
 const myBtn = document.getElementById('myBtn');
 window.onscroll =  () => {
@@ -25,13 +21,27 @@ function topFunc() {
 // ====================================================================================================
 
 
+// ====================================================================================================
+// Home-container
+  const header = document.querySelector('header');
+  const headerHeight = header.offsetHeight;
+  console.log("header height: " + headerHeight);
+  const headerStyle = window.getComputedStyle(header);
+  const headerMarginTop = parseInt(headerStyle.marginTop);
+  const headerMarginBottom = parseInt(headerStyle.marginBottom);
+  const totalHeaderHeight = headerHeight + headerMarginTop + headerMarginBottom;
 
+  console.log("total header height: " + totalHeaderHeight);
 
+  const headerRect = header.getBoundingClientRect();
+  if (headerRect.top === 0) {
+    document.body.style.paddingTop = `${totalHeaderHeight}px`;
+  }
 // ====================================================================================================
 // about-container
 
 const aboutContent = document.querySelector("#about-content p");
-aboutContent.innerHTML = "Hi! I am a pre-final year <b><i>computer science</i></b> student at <b><i>Quantum University, Roorkee</i></b>. My passion for computers and technology began at a young age, and it has been a driving force in my life ever since. I am dedicated to expanding my knowledge and skills in computer science, with a particular interest in software development and cyber security. Throughout my studies, I have actively sought out opportunities to work on various projects and further enhance my understanding of programming languages and algorithms.<br><br>Outside of my academic pursuits, I have also participated in coding competitions and hackathons, where I have had the opportunity to collaborate with peers and put my problem-solving abilities to the test. These experiences have not only strengthened my technical skills but have also provided me with valuable insights into working effectively within a team.<br><br> In addition to my technical interests, I am also passionate about contributing to the community. I have volunteered with local organizations to teach basic computer skills to underprivileged children, allowing me to share my knowledge and foster a greater appreciation for the potential of technology.<br><br> As I continue on my academic journey, I am committed to seizing every opportunity to further my skills, broaden my understanding, and ultimately make a positive impact in the field of computer science. I am excited about the possibilities that lie ahead and am eager to continue learning and growing as a computer scientist.";
+aboutContent.innerHTML = "Hi! I am a pre-final year <b><i>computer science</i></b> student at <b><i>Quantum University, Roorkee</i></b>. This enthusiasm has driven me to explore and deepen my knowledge in the field, particularly in software development and cybersecurity. My dedication to learning is reflected in my academic pursuits, where I have consistently sought opportunities to work on projects that enhance my understanding of programming languages, algorithms, and real-world problem-solving. <br><br>Beyond academics, I have actively participated in coding competitions and hackathons. These experiences have allowed me to collaborate with peers, tackle challenging problems, and strengthen both my technical and teamwork skills. They have also provided me with a platform to apply my knowledge in innovative and practical ways.<br><br> In addition to my technical interests, I am passionate about giving back to the community. I have volunteered with local organizations to teach basic computer skills to underprivileged children, sharing my knowledge and inspiring others to appreciate the transformative power of technology.<br><br> As I progress in my academic journey, I am committed to leveraging every opportunity to expand my skill set, deepen my understanding of computer science, and contribute meaningfully to the field. I am excited to embrace the challenges and opportunities that lie ahead and look forward to continuing my growth as a computer scientist while making a positive impact.";
 
 
 
